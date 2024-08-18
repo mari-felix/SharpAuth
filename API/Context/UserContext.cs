71db827e2eddafed637f5f8f2659c36f826ea27f
mari-felix <marianafelix.dev@gmail.com>
2024-08-18 15:18:48 -0300

    Updating project: changing UserContext config

--- API/Context/UserContext.cs
+++ API/Context/UserContext.cs
@@ -5,7 +5,10 @@ namespace API.Context
 {
     public class UserContext : DbContext
     {
-        public UserContext(DbContextOptions<UserContext> options) : base(options) {   }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;port=3306;database=users;user=root;password=root;", ServerVersion.AutoDetect("server=localhost;port=3306;database=users;user=root;password=root;"));
+        }
 
         public DbSet<User> Users { get; set;}
     }
