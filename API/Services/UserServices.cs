f304394529cf1bcfee3f1d4fbf9ad2103db7143b
mari-felix <marianafelix.dev@gmail.com>
2024-08-25 12:36:38 -0300

    Updating Project: new cryptographic and validation functions, starting log in function

--- API/Services/UserServices.cs
+++ API/Services/UserServices.cs
@@ -27,6 +27,84 @@ namespace API.Services
                 return false;
             }
         }
+
+        private bool IsValidPassword(string senha, string senhaDB) 
+        {
+           try 
+            {
+                return senha == senhaDB; 
+            }
+            catch 
+            {
+                return false;
+            } 
+        }
+
+        
+        private string HashPassword(string password) 
+        {
+            SHA256 sha256hash = SHA256.Create();
+
+            byte[] data = sha256hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sb.Append(data[i].ToString("x2"));
+            }
+            string hashPassword = sb.ToString();
+            return hashPassword;
+
+        }
+
+        private async Task<bool> CompararLog(string email, string password) 
+        {
+            try 
+            {
+                var usersDB = _userContext.Users;
+                if (IsValidEmail(email)) 
+                {
+                   var user = await usersDB.FirstOrDefaultAsync(x => x.Email == email);
+                   if (user != null) 
+                   {
+                        return IsValidPassword(password, user.Password);
+                   } 
+                   else
+                   {
+                        return false;
+                   }
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        } 
+
+        public async Task<User> LogarUsuario(string email, string password) 
+        {
+            try
+            {
+                var usersDB = _userContext.Users;
+                password = HashPassword(password);
+                var log = await CompararLog(email, password);
+                if (log == true)
+                {
+                    var user = await usersDB.FirstOrDefaultAsync(x => x.Email == email);
+                    return user;
+                } else {
+                    return null;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
         public async Task CriarUsuario(User user)
         {
             Random random = new Random();
@@ -34,21 +112,14 @@ namespace API.Services
 
             if (IsValidEmail(user.Email))
             {
-                using (SHA256 sha256hash = SHA256.Create()) 
-                {
-                    byte[] data = sha256hash.ComputeHash(Encoding.UTF8.GetBytes(user.Password));
-                    StringBuilder sb = new StringBuilder();
-                    for (int i = 0; i < data.Length; i++)
-                    {
-                        sb.Append(data[i].ToString("x2"));
-                    }
-                    string hashPassword = sb.ToString();
-                    user.Password = hashPassword;
-                }
+                
+                user.Password = HashPassword(user.Password);
 
                 _userContext.Users.Add(user);
                 await _userContext.SaveChangesAsync();
             }
+
+            user.CreatedAt = DateTime.Now;
         }
 
         public async Task<User> ObterUsuario(int id) 
